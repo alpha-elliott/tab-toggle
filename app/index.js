c31c11a2582d75cebf71a796e4f1eb9ea90c825e
Gurung <sgurung@ALPHA.local>
2016-10-27 11:43:53 -0700

    toggle h1 color on click

--- app/index.js
+++ app/index.js
@@ -1,15 +1,35 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var TextDisplay = React.createClass({
+  render: function() {
+    var newColor = this.props.color;
+    var style = {
+      color: newColor
+    }
+    return <h1 style={style}>{this.props.textA}</h1>
+  }
+});
+
 var HelloWorld = React.createClass({
+  getInitialState: function () {
+    return {color: 'blue'}
+  },
+  changeColor: function(){
+    color = this.state.color == 'blue' ? 'red' : 'blue';
+    this.setState({ color: color });
+  },
   render: function () {
     return (
-      <div> Hello ReactJS Program! </div>
+    <div>
+      <TextDisplay color={this.state.color} textA={this.props.textA}/>
+      <button onClick={this.changeColor}>Change Color</button>
+    </div>
     )
   }
 });
 
 ReactDOM.render(
-  <HelloWorld />,
+  <HelloWorld textA="Hello!"/>,
   document.getElementById('app')
 );
